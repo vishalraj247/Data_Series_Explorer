b23ebcf2fe247cc2bfbb1909ef80d737d8a16c34
zen-rs23 <k.rohitsharma2311@gmail.com>
2023-10-31 14:25:07 +1100

    Updated my Part B

--- tab_num/display.py
+++ tab_num/display.py
@@ -3,28 +3,13 @@ import streamlit as st
 from tab_num.logics import NumericColumn
 
 def display_tab_num_content(file_path=None, df=None):
-    """
-    --------------------
-    Description
-    --------------------
-    -> display_tab_num_content (function): Function that will instantiate tab_num.logics.NumericColumn class, save it into Streamlit session state and call its tab_num.logics.NumericColumn.find_num_cols() method in order to find all numeric columns.
-    Then it will display a Streamlit select box with the list of numeric columns found.
-    Once the user select a numeric column from the select box, it will call the tab_num.logics.NumericColumn.set_data() method in order to compute all the information to be displayed.
-    Then it will display a Streamlit Expander container with the following contents:
-    - the results of tab_num.logics.NumericColumn.get_summary() as a Streamlit Table
-    - the graph from tab_num.logics.NumericColumn.histogram using Streamlit.altair_chart()
-    - the results of tab_num.logics.NumericColumn.frequent using Streamlit.write
- 
-    --------------------
-    Parameters
-    --------------------
-    -> file_path (str): File path to uploaded CSV file (optional)
-    -> df (pd.DataFrame): Loaded dataframe (optional)
-
-    --------------------
-    Returns
-    --------------------
-    -> None
-
-    """
+    num_col_instance = NumericColumn(file_path=file_path, df=df)
+    num_col_instance.find_num_cols()
+    selected_num_col = st.selectbox("Select Numeric Column", num_col_instance.cols_list)
     
+    if selected_num_col:
+        num_col_instance.set_data(selected_num_col)
+        with st.expander("Column Summary"):
+            st.table(num_col_instance.get_summary())
+            st.altair_chart(num_col_instance.histogram)
+            st.write(num_col_instance.frequent)
--- tab_num/logics.py
+++ tab_num/logics.py
@@ -46,322 +46,110 @@ class NumericColumn:
         self.histogram = alt.Chart()
         self.frequent = pd.DataFrame(columns=['value', 'occurrence', 'percentage'])
 
+    
     def find_num_cols(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> find_num_cols (method): Class method that will load the uploaded CSV file as Pandas DataFrame and store it as attribute (self.df) if it hasn't been provided before.
-        Then it will find all columns of numeric data type and store the results in the relevant attribute (self.cols_list).
+        if self.file_path is not None:
+            self.file_path.seek(0)
+            data_df = pd.read_csv(self.file_path, low_memory=False)
 
-        --------------------
-        Parameters
-        --------------------
-        -> None
+        list_of_num_txt_columns = []
+        for col in data_df.columns:
+            if data_df[col].dtype in ['float64', 'int64']:
+                list_of_num_txt_columns.append(col)
+            elif data_df[col].dtype == 'object':
+                temp_series = pd.to_numeric(data_df[col], errors='coerce')
+                # Check if all values are numeric after conversion
+                if not temp_series.isnull().all():  # Ensure not all values are NaN after conversion
+                    data_df[col] = temp_series
+                    list_of_num_txt_columns.append(col)
 
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
+        self.df = data_df
+        self.cols_list = list_of_num_txt_columns
 
     def set_data(self, col_name):
-        """
-        --------------------
-        Description
-        --------------------
-        -> set_data (method): Class method that sets the self.serie attribute with the relevant column from the dataframe and then computes all requested information from self.serie to be displayed in the Numeric section of Streamlit app 
-
-        --------------------
-        Parameters
-        --------------------
-        -> col_name (str): Name of the numeric column to be analysed
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
+        self.serie = self.df[col_name]
+        self.convert_serie_to_num()
+        self.set_unique()
+        self.set_missing()
+        self.set_zeros()
+        self.set_negatives()
+        self.set_mean()
+        self.set_std()
+        self.set_min()
+        self.set_max()
+        self.set_median()
+        self.set_histogram()
+        self.set_frequent()
 
     def convert_serie_to_num(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> convert_serie_to_num (method): Class method that convert a Pandas Series to numeric data type and store the results in the relevant attribute (self.serie).
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
+        self.serie = pd.to_numeric(self.serie, errors='coerce')
 
     def is_serie_none(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> is_serie_none (method): Class method that checks if self.serie is empty or none and store the results in the relevant attribute (self.cols_list) if self.serie is not empty nor None
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> (bool): Flag stating if the serie is empty or not
-
-        """
-        
+        return self.serie is None or self.serie.empty
 
     def set_unique(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> set_unique (method): Class method that computes the number of unique value of a column and store the results in the relevant attribute (self.n_unique) if self.serie is not empty nor None
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
+        if not self.is_serie_none():
+            self.n_unique = self.serie.nunique()
 
     def set_missing(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> set_missing (method): Class method that computes the number of missing value of a serie and store the results in the relevant attribute (self.n_missing) if self.serie is not empty nor None
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
+        if not self.is_serie_none():
+            self.n_missing = self.serie.isnull().sum()
 
     def set_zeros(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> set_zeros (method): Class method that computes the number of times a serie has values equal to 0 and store the results in the relevant attribute (self.n_zeros) if self.serie is not empty nor None
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
+        if not self.is_serie_none():
+            self.n_zeros = (self.serie == 0).sum()
 
     def set_negatives(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> set_negatives (method): Class method that computes the number of times a serie has negative values and store the results in the relevant attribute (self.n_negatives) if self.serie is not empty nor None
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
+        if not self.is_serie_none():
+            self.n_negatives = (self.serie < 0).sum()
 
     def set_mean(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> set_mean (method): Class method that computes the average value of a serie and store the results in the relevant attribute (self.col_mean) if self.serie is not empty nor None
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
+        if not self.is_serie_none():
+            self.col_mean = self.serie.mean()
 
     def set_std(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> set_std (method): Class method that computes the standard deviation value of a serie and store the results in the relevant attribute (self.col_std) if self.serie is not empty nor None
+        if not self.is_serie_none():
+            self.col_std = self.serie.std()
 
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
-    
     def set_min(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> set_min (method): Class method that computes the minimum value of a serie and store the results in the relevant attribute (self.col_min) if self.serie is not empty nor None
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
+        if not self.is_serie_none():
+            self.col_min = self.serie.min()
 
     def set_max(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> set_max (method): Class method that computes the maximum value of a serie and store the results in the relevant attribute (self.col_max) if self.serie is not empty nor None
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
+        if not self.is_serie_none():
+            self.col_max = self.serie.max()
 
     def set_median(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> set_median (method): Class method that computes the median value of a serie and store the results in the relevant attribute (self.col_median) if self.serie is not empty nor None
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
+        if not self.is_serie_none():
+            self.col_median = self.serie.median()
 
     def set_histogram(self):
-        """
-        --------------------
-        Description
-        --------------------
-        -> set_histogram (method): Class method that computes the Altair histogram displaying the count for each bin value of a serie and store the results in the relevant attribute (self.histogram) if self.serie is not empty nor None
+        if not self.is_serie_none():
+            # Convert series to DataFrame for Altair
+            data_for_histogram = self.serie.reset_index(drop=True).to_frame('value')
 
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
+            # Create an Altair histogram chart
+            self.histogram = alt.Chart(data_for_histogram).mark_bar().encode(
+                alt.X("value:Q", bin=True),
+                y='count()',
+            ).interactive()  # making it interactive
 
     def set_frequent(self, end=20):
-        """
-        --------------------
-        Description
-        --------------------
-        -> set_frequent (method): Class method that computes the Dataframe containing the most frequest value of a serie and store the results in the relevant attribute (self.frequent) if self.serie is not empty nor None
+        if not self.is_serie_none():
+            value_counts = self.serie.value_counts().head(end).reset_index()
+            value_counts.columns = ['value', 'occurrence']
+            value_counts['percentage'] = (value_counts['occurrence'] / len(self.serie)) * 100
+            
+            # Using pandas' Styler to format the percentage column
+            self.frequent = value_counts.style.format({
+                'percentage': '{:,.2f}%'
+            })
 
-        --------------------
-        Parameters
-        --------------------
-        -> end (int):
-            Parameter indicating the maximum number of values to be displayed
-
-        --------------------
-        Returns
-        --------------------
-        -> None
-
-        """
-        
-    def get_summary(self,):
-        """
-        --------------------
-        Description
-        --------------------
-        -> get_summary_df (method): Class method that formats all requested information from self.serie to be displayed in the Overall section of Streamlit app as a Pandas dataframe with 2 columns: Description and Value
-
-        --------------------
-        Parameters
-        --------------------
-        -> None
-
-        --------------------
-        Returns
-        --------------------
-        -> (pd.DataFrame): Formatted dataframe to be displayed on the Streamlit app
-
-        """
-        
+    def get_summary(self):
+        data = {
+            'Description': ['Number of Unique Values', 'Number of Rows with Missing Values', 'Number of Rows with 0', 'Number of Rows with Negative Values',
+                            'Average value', 'Standard Deviation Value', 'Minimum Value', 'Maximum Value', 'Median Value'],
+            'Value': [self.n_unique, self.n_missing, self.n_zeros, self.n_negatives, self.col_mean, 
+                      self.col_std, self.col_min, self.col_max, self.col_median]
+        }
+        return pd.DataFrame(data)
