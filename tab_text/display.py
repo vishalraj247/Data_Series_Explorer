3ab2d5e5bc62b80648417b0186bd0da44bb03e56
ronikj <122254894+ronikj@users.noreply.github.com>
2023-11-03 09:26:03 +1100

    Update display.py

--- tab_text/display.py
+++ tab_text/display.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-from tab_text.logics import TextColumn
+from logics import TextColumn
+import pandas as pd
 
 def display_tab_text_content(file_path=None, df=None):
     """
@@ -27,4 +28,28 @@ def display_tab_text_content(file_path=None, df=None):
     -> None
 
     """
-    
+
+    if file_path is not None:
+        df = pd.read_csv(file_path)
+        text_column = TextColumn(df=df)
+        text_column.find_text_cols()
+
+        selected_column = st.selectbox('Which text column do you want to explore', text_column.cols_list)
+
+        if selected_column:
+            text_column.set_data(selected_column)
+
+            with st.expander('Text Column'):
+                st.table(text_column.get_summary())
+
+                st.subheader('Bar Chart')
+                st.altair_chart(text_column.barchart, use_container_width=True)
+
+                st.subheader('Most Frequent Values')
+                st.table(text_column.frequent)
+
+st.title('Text Column Analysis')
+
+uploaded_file =st.file_uploader('Choose a csv file', type=['csv'])
+if uploaded_file is not None:
+    display_tab_text_content(file_path=uploaded_file)
