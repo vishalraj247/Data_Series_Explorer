c1f930ffc385b334ebec35b1b140abe31dbf7914
ronikj <122254894+ronikj@users.noreply.github.com>
2023-11-06 11:25:02 +1100

    Update display.py

--- tab_text/display.py
+++ tab_text/display.py
@@ -1,30 +1,25 @@
 import streamlit as st
 
-from logics import TextColumn
-import pandas as pd
+from text_logics import TextColumn
+
 
 def display_tab_text_content(file_path=None, df=None):
-    if file_path is not None:
-        df = pd.read_csv(file_path)
-        text_column = TextColumn(df=df)
-        text_column.find_text_cols()
+    text_column = TextColumn(file_path=file_path, df=df)
+    text_column.find_text_cols()
 
-        selected_column = st.selectbox('Which text column do you want to explore', text_column.cols_list)
+    selected_text_column = st.selectbox('Which text column do you want to explore', text_column.cols_list)
 
-        if selected_column:
-            text_column.set_data(selected_column)
+    if selected_text_column:
+        text_column.set_data(selected_text_column)
 
-            with st.expander('Text Column'):
-                st.table(text_column.get_summary())
+        with st.expander('Text Column', expanded=True):
+            st.table(text_column.get_summary())
 
-                st.subheader('Bar Chart')
-                st.altair_chart(text_column.barchart, use_container_width=True)
+            st.subheader('Bar Chart')
+            st.altair_chart(text_column.barchart, use_container_width=True)
 
-                st.subheader('Most Frequent Values')
-                st.table(text_column.frequent)
+            st.subheader('Most Frequent Values')
+            st.table(text_column.frequent)
+
 
 st.title('Text Column Analysis')
-
-uploaded_file =st.file_uploader('Choose a csv file', type=['csv'])
-if uploaded_file is not None:
-    display_tab_text_content(file_path=uploaded_file)
