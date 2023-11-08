c4c9ebd32c331739d5381a7d00efba4f7cfeff2a
ronikj <122254894+ronikj@users.noreply.github.com>
2023-11-09 03:01:25 +1100

    Update display.py

--- tab_text/display.py
+++ tab_text/display.py
@@ -8,15 +8,20 @@ def display_tab_text_content(file_path=None, df=None):
     text_column.find_text_cols()
 
     selected_text_column = st.selectbox('Which text column do you want to explore', text_column.cols_list)
+    if text_cols_list is not None:
+        selected_text_column = st.selectbox('Which text column do you want to explore', text_cols_list)
 
-    if selected_text_column:
-        text_column.set_data(selected_text_column)
+        if selected_text_column:
+            text_column.set_data(selected_text_column)
 
-        with st.expander('Text Column', expanded=True):
-            st.table(text_column.get_summary())
+            with st.expander('Text Column', expanded=True):
+                st.table(text_column.get_summary())
 
-            st.subheader('Bar Chart')
-            st.altair_chart(text_column.barchart, use_container_width=True)
+            with st.expander('Bar Chart', expanded=True):
+                st.altair_chart(text_column.barchart, use_container_width=True)
 
-            st.subheader('Most Frequent Values')
-            st.table(text_column.frequent)
+            with st.expander('Most Frequent Values', expanded=True):
+                st.table(text_column.frequent)
+
+    else:
+        st.error('No text columns available')
